ff46a9166f800954b99d1a5ad2dc82e1b8ad15e9
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2022-07-03 01:07:06 +0530

    Add files via upload

--- oops.java
+++ oops.java
@@ -1,13 +1,41 @@
+package com.company;
+
+class Employee{
+    int id;
+    int salary;
+    String name;
+    public void printDetails(){
+        System.out.println("My id is " + id);
+        System.out.println("and my name is "+ name);
+    }
+
+    public int getSalary(){
+        return salary;
+    }
+}
+
 public class oops {
     public static void main(String[] args) {
-        /* object oriented programming tries to map code instructions with real world
-        making the code short and easier to understand.
-        solving a problem by creating object is called the object oriented programming.
-        an object is a instantation of a class when
-        a class is defined a template is defined .memory is allocated only after the object instantiation.
+        System.out.println("This is our custom class");
+        Employee harry = new Employee(); // Instantiating a new Employee Object
+        Employee john = new Employee(); // Instantiating a new Employee Object
 
+        // Setting Attributes for Harry
+        harry.id = 12;
+        harry.salary = 34;
+        harry.name = "CodeWithHarry";
 
-         */
+        // Setting Attributes for John
+        john.id = 17;
+        john.salary = 12;
+        john.name = "John Khandelwal";
 
+        // Printing the Attributes
+        harry.printDetails();
+        john.printDetails();
+        int salary = john.getSalary();
+        System.out.println(salary);
+        // System.out.println(harry.id);
+        // System.out.println(harry.name);
     }
 }
