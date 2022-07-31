f2cac26a7678ebefc6ee93eb23b306b0107d709c
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2022-08-01 01:09:47 +0530

    Create Convert String to LowerCase.java

---
+++ Convert String to LowerCase.java
@@ -0,0 +1,20 @@
+/*
+Given a string S. The task is to convert characters of string to lowercase.
+
+Example 1:
+
+Input: S = "ABCddE"
+Output: "abcdde"
+Explanation: A, B, C and E are converted to
+a, b, c and E thus all uppercase characters 
+of the string converted to lowercase letter.
+*/
+
+//code'
+
+
+class Solution {
+    static String toLower(String S) {
+        return S.toLowerCase();
+    }
+}
