406a7b9ebcfb49ac27c55eaa04ed38cac10e323c
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2022-11-03 00:26:30 +0530

    Create odd even.java

---
+++ odd even.java
@@ -0,0 +1,17 @@
+package JAVA_QUESTIONS;
+import java.util.Scanner;
+public class Odd_even {
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	if(a%2==0) {
+		System.out.println("even");
+		}
+	else {
+		System.out.println("odd");
+	}
+	}
+}
+	
+
+
